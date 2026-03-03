29e1d6272f9300d1ff40334928e651214b9cea81
Ian Lam <ljy200523@gmail.com>
2026-03-03 17:09:28 +0800

    Complete grid and hover with incomplete button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,3 +1,31 @@
 
-const column = document.createElement("div");
-const row = document.createElement("div");
+const numberOfSquares = document.querySelector("numberOfSquares");
+console.log(numberOfSquares);
+
+const container = document.querySelector("#container");
+for (let i = 0; i < numberOfSquares; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+    for (let j = 0; j < numberOfSquares; j++) {
+        const column = document.createElement("div");
+        column.classList.add("column");
+        row.appendChild(column);
+    }
+}
+
+const totalDivs = document.querySelectorAll(".column");
+totalDivs.forEach(divs => {
+    divs.addEventListener("mouseenter", function(event) {
+        event.target.style.backgroundColor = "blue";
+        console.log("Mouse Entered");
+        console.log(event.currentTarget);
+    });
+    divs.addEventListener("mouseleave", function(event) {
+        event.target.style.backgroundColor = "";
+        console.log("Mouse Left");
+    });
+});
+
+console.log("after");
+

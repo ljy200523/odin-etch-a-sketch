d72520ee5ea8a888d58eaf815b3d2791268305c3
Ian Lam <ljy200523@gmail.com>
2026-03-04 10:08:01 +0800

    Complete prompt and changing of grid size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,35 +1,47 @@
 
 const initialSqaures = 16;
+let numberOfSquares = initialSqaures;
 
 const container = document.querySelector("#container");
-for (let i = 0; i < initialSqaures; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    for (let j = 0; j < initialSqaures; j++) {
-        const column = document.createElement("div");
-        column.classList.add("column");
-        row.appendChild(column);
+
+function refreshGrid() {
+    container.innerHTML = '';
+    for (let i = 0; i < numberOfSquares; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        row.style.height = `${100/numberOfSquares}%`;
+        container.appendChild(row);
+        for (let j = 0; j < numberOfSquares; j++) {
+            const column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);
+        }
     }
+    const totalDivs = document.querySelectorAll(".column");
+    totalDivs.forEach(divs => {
+        divs.addEventListener("mouseenter", function(event) {
+            event.target.style.backgroundColor = "blue";
+            console.log("Mouse Entered");
+            console.log(event.currentTarget);
+        });
+        divs.addEventListener("mouseleave", function(event) {
+            event.target.style.backgroundColor = "";
+            console.log("Mouse Left");
+        });
+    });
 }
 
-const totalDivs = document.querySelectorAll(".column");
-totalDivs.forEach(divs => {
-    divs.addEventListener("mouseenter", function(event) {
-        event.target.style.backgroundColor = "blue";
-        console.log("Mouse Entered");
-        console.log(event.currentTarget);
-    });
-    divs.addEventListener("mouseleave", function(event) {
-        event.target.style.backgroundColor = "";
-        console.log("Mouse Left");
-    });
-});
+refreshGrid()
+
 
 const submitButton = document.querySelector("button");
-submitButton.addEventListener("click", function(value) {
-    const numberOfSquares = document.querySelector('input').value;
+submitButton.addEventListener("click", function(event) {
+    numberOfSquares = prompt("Number of squares: ");
+    if (numberOfSquares > 100) numberOfSquares = 100;
+    else if (numberOfSquares < 1) numberOfSquares = 1;
+    event.preventDefault();
     console.log(numberOfSquares);
+    refreshGrid();
 })
 
 console.log("after");
